d61bd3059a8996f692afbb621ec1514a4bec90fb
JessicaCtrn <jessicacatherine_2327240010@mhs.mdp.ac.id>
2025-06-02 14:51:33 +0700

    save

--- routes/web copy.php
+++ routes/web copy.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FakultasController;
 use App\Http\Controllers\JadwalController;
@@ -10,26 +7,21 @@ use App\Http\Controllers\ProdiController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\MatakuliahController;
 use App\Http\Controllers\SesiController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::get('/profil', function () { //utk di web
+    return view ('profil'); //samain kek view
 });
+ 
 
-require __DIR__.'/auth.php';
 Route::resource('/fakultas', FakultasController::class);
 Route::resource('/prodi', ProdiController::class);
 Route::resource('/mahasiswa', MahasiswaController::class);
 Route::resource('/matakuliah', MatakuliahController::class);
 Route::resource('/jadwal', JadwalController::class);
 Route::resource('/sesi', SesiController::class);
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index']);
